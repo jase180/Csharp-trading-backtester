b92867881459d090e7bcc65a85058802c78c9bb8
Jason <jase180@gmail.com>
2025-09-21 11:05:12 -0500

    feat: Add iStrategy the itnerface for all strategies

---
+++ Strategies/IStrategy.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using TradingBacktester.Models;
+
+namespace TradingBacktester.Strategies
+{
+    /// <summary>
+    /// Interface for all trading strategies
+    /// NOTETOSELF C# interfacing is NOT IMPLICIT LIKE GOLANG
+    /// To be added after MVP: Configs
+    /// </summary>
+    public interface IStrategy
+    {
+        /// <summary>
+        /// Generate trading signals based on price data
+        /// </summary>
+        /// <param name="prices">Historical price data</param>
+        /// <returns>List of buy/sell signals</returns>
+        List<TradeSignal> GenerateSignals(List<Price> prices);
+
+        /// <summary>
+        /// Name of this strategy (for reporting)
+        /// </summary>
+        string StrategyName { get; }
+    }
+}
